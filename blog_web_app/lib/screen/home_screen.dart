3de49412922c0b9f39310d2a140ce98b618bbe5a
DH <psycn@naver.com>
2024-10-06 22:47:48 +0900

    blog_web_app 완료

--- blog_web_app/lib/screen/home_screen.dart
+++ blog_web_app/lib/screen/home_screen.dart
@@ -4,7 +4,7 @@ import 'package:webview_flutter/webview_flutter.dart';
 class HomeScreen extends StatelessWidget{
   WebViewController webViewController = WebViewController()
 
-  ..loadRequest(Uri.parse('https://blog.codefactory.ai/'))
+    ..loadRequest(Uri.parse('https://blog.codefactory.ai/'))
 
     // 자바스크립트 실행가능
   ..setJavaScriptMode(JavaScriptMode.unrestricted);
@@ -20,6 +20,30 @@ class HomeScreen extends StatelessWidget{
         title: Text('Blog Web App'),
 
         centerTitle: true,
+
+        // AppBar에 액션 버튼을 추가할 수 있는 매개변수
+        actions: [
+          IconButton(
+              onPressed: (){webViewController.goBack();},
+              icon: Icon(
+                Icons.arrow_back
+              ),
+          ),
+          IconButton(
+            onPressed: (){
+            webViewController.loadRequest(Uri.parse('https://blog.codefactory.ai'));
+            },
+            icon: Icon(
+              Icons.home
+            ),
+          ),
+          IconButton(
+              onPressed: (){webViewController.goForward();},
+              icon: Icon(
+                  Icons.arrow_forward
+              )
+          ),
+        ],
       ),
       body: WebViewWidget(
         controller: webViewController,
